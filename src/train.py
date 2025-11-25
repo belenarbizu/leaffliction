e833b95e0083182c8b7c9c37eee2d4ecf20a01c3
barbizu <belenarbizu98@gmail.com>
2025-11-25 18:38:42 +0100

    zip done

--- src/train.py
+++ src/train.py
@@ -4,12 +4,16 @@ import tensorflow as tf
 import warnings
 from utils import get_directory
 from tensorflow.keras import layers, models
+from pathlib import Path
+import pandas as pd
+import zipfile
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 warnings.filterwarnings("ignore")
 
 
 def check_dir(directory):
-    subdirs = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
+    subdirs = [os.path.join(directory, d) for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
     if len(subdirs) < 2:
         print("The directory must contain at least two subdirectories for classification.")
         exit(1)
@@ -19,23 +23,12 @@ def split_dataset(data_dir, split_ratio):
     """
     Split the dataset into training and validation sets based on the given ratio.
     """
-    train_df = tf.keras.preprocessing.image_dataset_from_directory(
+    train_df, val_df = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
         validation_split=1 - split_ratio,
-        subset="training",
+        subset="both",
         seed=42,
-        image_size=(256, 256),
-        # batch_size=32
-    )
-    shape = tf.TensorShape([None,140,140,3])
-
-    val_df = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=1 - split_ratio,
-        subset="validation",
-        seed=42,
-        image_size=(256, 256),
-        # batch_size=32
+        image_size=(128, 128)
     )
     return train_df, val_df
 
@@ -50,11 +43,11 @@ def train(train_data, val_data):
         layers.MaxPooling2D((2, 2)),
         layers.Conv2D(64, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.Conv2D(32, (1, 1), activation='relu'),
         layers.MaxPooling2D((2, 2)),
         layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(128, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(256, activation='relu'),
         layers.Dense(len(train_data.class_names), activation='softmax')
     ])
 
@@ -62,8 +55,19 @@ def train(train_data, val_data):
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(train_data, validation_data=val_data, epochs=4)
+    model.fit(train_data, validation_data=val_data, epochs=3)
     return model
+
+
+def create_zip(model, class_names):
+    model.save("model/model.h5")
+    pd.DataFrame({'class_names': class_names}).to_csv("model/class_names.csv", index=False)
+
+    with zipfile.ZipFile("model.zip", "w") as zipf:
+        zipf.write("model/model.h5")
+        zipf.write("model/class_names.csv")
+
+    os.system("sha1sum model.zip > model/sha1sum.txt")
 
 
 def main():
@@ -81,7 +85,8 @@ def main():
     model = train(train_data, val_data)
     results = model.evaluate(val_data)
     print(f"Validation Loss: {results[0]}, Validation Accuracy: {results[1]}")
-    model.save("model/model.h5")
+
+    create_zip(model, train_data.class_names)
 
 
 if __name__ == "__main__":
