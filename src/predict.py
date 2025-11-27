9ba00f25c415580c23a34a472e6b03a0ac2cde7e
barbizu <belenarbizu98@gmail.com>
2025-11-27 17:27:33 +0100

    predict done

--- src/predict.py
+++ src/predict.py
@@ -8,26 +8,38 @@ import numpy as np
 
 
 def check_path(image_path):
+    """
+    Check if the provided image path is valid.
+    """
     try:
         path = Path(image_path)
-        if not path.is_file():
-            print(f"Error: The specified image file does not exist: {image_path}")
+        if not path.exists() or not path.is_file():
+            print(f"Error: The specified path is not a file or does not exist: {image_path}.")
+            exit(1)
     except Exception as e:
         print(f"Error: Invalid path provided: {image_path}.")
         exit(1)
 
 
-def open_model():
+def open_model(model_zip_path):
+    """
+    Extract the model and class names from the zip file.
+    Returns the paths to the model file and class names file.
+    """
     try:
-        with zipfile.ZipFile("model.zip", "r") as zip_ref:
+        with zipfile.ZipFile(model_zip_path, "r") as zip_ref:
             zip_ref.extractall(".")
     except Exception as e:
         print(f"Error extracting model: {e}")
         exit(1)
-    return "model/model.h5", "model/class_names.csv"
+    return f"model/{model_zip_path.split('_model.zip')[0]}_model.h5", f"model/{model_zip_path.split('_model.zip')[0]}_class_names.csv"
 
 
 def load_class_names(class_names_path):
+    """
+    Load class names from the CSV file.
+    Returns a list of class names.
+    """
     try:
         df = pd.read_csv(class_names_path)
         class_names = df.iloc[:, 0].tolist()
@@ -38,7 +50,11 @@ def load_class_names(class_names_path):
 
 
 def predict_image(image_path, model_path, class_names):
+    """
+    Predict the class of the given image using the loaded model.
+    """
     model = load_model(model_path)
+
     img = image.load_img(image_path, target_size=(128, 128))
     img_array = image.img_to_array(img)
     _ = img_array / 255.0  # Normalize the image
@@ -48,16 +64,18 @@ def predict_image(image_path, model_path, class_names):
     predicted_class_index = np.argmax(predictions[0])
     confidence = predictions[0][predicted_class_index]
     predicted_class_name = class_names[predicted_class_index]
+
     print(f"Predicted class: {predicted_class_name} with confidence {confidence:.2f}")
 
 
 def main():
     argparser = argparse.ArgumentParser(description="Predict using a trained model.")
     argparser.add_argument(type=str, dest="image", help="Path to the image file to classify.")
+    argparser.add_argument("-m", "--model", required=True, type=str, dest="model", help="Path to the model zip file.")
     args = argparser.parse_args()
 
     check_path(args.image)
-    model_path, class_names_path = open_model()
+    model_path, class_names_path = open_model(args.model)
     class_names = load_class_names(class_names_path)
     if not class_names:
         print("No class names found.")
